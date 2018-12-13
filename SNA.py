decc6d8c4218fcb6c9225ab15788bcbdc132894c
hrdkdh <36975040+hrdkdh@users.noreply.github.com>
2018-12-13 11:16:09 +0900

    Update SNA.py

--- SNA.py
+++ SNA.py
@@ -1,11 +1,11 @@
 #-*- coding: utf-8 -*-
 
 """
-관계 분석 플로우는 아래와 같다
+관계 분석을 위한 데이터 처리 플로우는 아래와 같다
 1. 구성원 한명씩 친밀도를 물어보는 형태로 데이터를 수집한다.
 2. 피설문자별 응답결과의 평균과 표준편차를 구해 표준점수화 한다.
-3. 표준점수화 하면 음수가 나오므로, 전체 데이터 테이블의 최소값을 각 데이터포인터에 더해주는 형태로 하여 모든 데이터를 양수로 변환한다.
-4. 3번 데이터 테이블에서 나온 전체 평균값보다 낮은 점수는 모두 0점으로 처리한다.
+3. 표준점수화 하면 평균(0점) 이하점수는 음수가 나오며, 음수는 모두 0점으로 처리하여 단순화한다.
+4. 그래프를 그린다.
 """
 
 import pandas as pd
@@ -13,7 +13,7 @@ import pandas as pd
 #데이터 로드
 df = pd.read_csv("data/network_test_data.csv")
 
-def getNetworkGraph(df):
+def getNetworkGraph(df, figsize=(12,12), dpi=160, nodeColor="#05507d", arrowDefaultColor="#dddddd", arrowEmpathyzedColor="#ff708a"):
     import networkx as nx
     import matplotlib.pyplot as plt
 
@@ -21,7 +21,7 @@ def getNetworkGraph(df):
     matrix=df.values
 
     #플롯 설정
-    plt.figure(figsize=(12, 12), dpi=160, facecolor="w")
+    plt.figure(figsize=figsize, dpi=dpi, facecolor="w")
     plt.axis("off")
     
     #플롯 한글폰트 설정
@@ -68,9 +68,9 @@ def getNetworkGraph(df):
             if edge1[0]==edge2[1] and edge1[1]==edge2[0]:
                 directCheck=1
         if directCheck==1:
-            edge_color_list.append("#ff708a")
+            edge_color_list.append(arrowEmpathyzedColor)
         else:
-            edge_color_list.append("#cccccc")
+            edge_color_list.append(arrowDefaultColor)
 
     #print(edge_list)
     #print(edge_color_list)
@@ -79,9 +79,9 @@ def getNetworkGraph(df):
     #k는 노드 간의 최소 거리, iteration은 최적점을 찾는 반복횟수, 스케일은 크기
     pos=nx.spring_layout(DG, k=3, iterations=100, scale=2)
     nx.draw_networkx_edges(DG, pos, edgelist=edge_list, node_size=node_size_default-100, edge_color=edge_color_list, width=2, arrows=True)
-    nx.draw_networkx_nodes(DG, pos, nodelist=node_list, node_size=node_size_default, node_color="#05507d")
+    nx.draw_networkx_nodes(DG, pos, nodelist=node_list, node_size=node_size_default, node_color=nodeColor)
     nx.draw_networkx_labels(DG, pos, font_family="NanumGothic", font_size=12, font_color="white", font_weight="bold")
     #nx.draw_networkx_edge_labels(DG, pos, edge_labels=edge_labels, alpha=0.45, font_size=5)
     
 #그래프 생성
-getNetworkGraph(df)
+getNetworkGraph(df, nodeColor="#00a651")
